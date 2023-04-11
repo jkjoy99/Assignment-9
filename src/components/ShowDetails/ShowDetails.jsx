d82a4da05a5f19031ca27cafff4a7b1e3c6e94eb
jkjoy99 <jkjoy99@gmail.com>
2023-04-11 18:08:55 +0600

    local staroz saved

--- src/components/ShowDetails/ShowDetails.jsx
+++ src/components/ShowDetails/ShowDetails.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import { useLoaderData, useParams } from 'react-router-dom';
 import './ShowDetails.css'
+import { addToDb } from '../../utils/fakeDb';
 
 const ShowDetails = () => {
+
+    const handleAddToCart = id => {
+        console.log(id)
+        addToDb(id)
+    }
 
     const jobData = useLoaderData();
     const {id} = useParams();
@@ -11,13 +17,15 @@ const ShowDetails = () => {
 
     const jobDetailsData = jobData.find(item => item.id === id);
     // console.log(jobDetailsData);
-    const { jobDescription, jobResponsibilities, educationRequirements, experience, salary, jobTitle, phoneNumber, email, location } = jobDetailsData;
+    const { jobDescription, jobResponsibilities, educationRequirements, experience, salary, jobTitle, phoneNumber, email, location, } = jobDetailsData;
 
-    return (
+   
+
+    return (               
         <div>
             <h2 className='text-2xl text-black font-semibold text-2xl'>Job Details</h2>
         <div className='flex mt-14'>
-            <div className='w-2/4 mx-20'>
+            <div className='w-2/4 mx-20 tracking-wide'>
             <p> <span className='font-semibold text-2xl'>Job Description:</span>  {jobDescription}</p>
             <p> <span className='font-semibold text-2xl'> Job Responsibility:</span>{jobResponsibilities}</p>
             <p> <span className='font-semibold text-2xl'> Educational Requirements:</span>{educationRequirements}</p>
@@ -33,7 +41,8 @@ const ShowDetails = () => {
                <p> <span className='font-semibold text-2xl'>Phone :</span>{phoneNumber}</p>
                <p> <span className='font-semibold text-2xl'>Email: </span>{email}</p>
                <p> <span className='font-semibold text-2xl'>Address :</span>{location}</p>
-               <button className='bg-blue-700 py-2 px-2 rounded text-white hover:bg-blue-900 text-center mt-5 '>Apply Now</button>
+               <button className='bg-blue-700 py-2 px-2 rounded text-white hover:bg-blue-900 text-center mt-5 w-full block 
+               ' onClick={() => handleAddToCart(id)}>Apply Now</button>
             </div>
         </div>
         </div>
---
+++ src/utils/fakeDb.js
@@ -0,0 +1,22 @@
+// add data to local storage
+
+const addToDb = id=> {
+let FeatureJobs = {}
+const storedJobs = localStorage.getItem('FeatureJobs')
+if(storedJobs){
+    FeatureJobs = JSON.parse(storedJobs)
+}
+
+const quantity = FeatureJobs[id]
+if(quantity){
+const newQuantity = quantity + 1
+FeatureJobs[id] = newQuantity
+}
+else{
+    FeatureJobs[id] = 1
+}
+localStorage.setItem('FeatureJobs', JSON.stringify(FeatureJobs))
+}
+
+
+export {addToDb}
