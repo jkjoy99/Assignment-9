771430bbb813fb1e58735e59ca32bca5d0d407a8
jkjoy99 <jkjoy99@gmail.com>
2023-04-12 13:27:35 +0600

    see all btn aply

--- src/components/FeaturedJob/FeaturedJob.jsx
+++ src/components/FeaturedJob/FeaturedJob.jsx
@@ -26,7 +26,7 @@ const handleShowAll = () => {
 
         {!showAll && 
         <div onClick={handleShowAll}>
-          <Button>See All Jobs</Button>
+          <button className='bg-blue-700 py-2 px-2 rounded text-white hover:bg-blue-900 text-center mt-5 w-full block '>See All Jobs</button>
         </div>
       }  
        </section>
